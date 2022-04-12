65a348f741e6f1a99bd34f68807a73e1b759039d
Matti <matti.kahkonen99@gmail.com>
2022-04-12 14:32:33 +0300

    Update .jar file

--- DDSheet/src/main/java/ddsheet/domain/DDSheetService.java
+++ DDSheet/src/main/java/ddsheet/domain/DDSheetService.java
@@ -4,15 +4,15 @@ package ddsheet.domain;
 import java.util.HashMap;
 
 public class DDSheetService {
-    
+
     //hashmapin käyttö on väliaikaista, myöhemmin vaihdetaan sqliteen
     final private HashMap<String, User> users = new HashMap();
     private User loggedUser;
-    
+
     public DDSheetService() {
-        
+
     }
-    
+
     public String attemptCreateUser(String username, String password) {
         if (existingUsername(username)) {
             return "Username already taken";
@@ -26,7 +26,7 @@ public class DDSheetService {
         users.put(username, new User(username, password));
         return "Account successfully created!";
     }
-    
+
     public String attemptLogIn(String username, String password) {
         if (!existingUsername(username)) {
             return "There is no account with that username";
@@ -37,28 +37,28 @@ public class DDSheetService {
         loggedUser = users.get(username);
         return ("Login successful!");
     }
-    
+
     public void logOut() {
         loggedUser = null;
     }
-    
+
     public boolean existingUsername(String username) {
         return users.containsKey(username);
     }
-    
+
     public boolean correctPassword(String username, String password) {
         User user = users.get(username);
         return (password.equals(user.getPassword()));
     }
-    
+
     public boolean usernameLongEnough(String username) {
-        return (3 < username.length());
+        return (3 <= username.length());
     }
-    
+
     public boolean passwordLongEnough(String password) {
-        return (3 < password.length());
+        return (3 <= password.length());
     }
-    
+
     public String addCharacter(String name) {
         if (characterNameLongEnough(name)) {
             loggedUser.getCharacters().add(new Character(name));
@@ -66,13 +66,13 @@ public class DDSheetService {
         }
         return "Name must be at least one character long";
     }
-    
+
     public boolean characterNameLongEnough(String name) {
         return 0 < name.length();
     }
-    
+
     public User getUser() {
         return loggedUser;
     }
-    
+
 }
